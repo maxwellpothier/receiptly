e5671a06e6101a3d716eab99696845939a85fcb0
maxwellpothier <pothiermaxwell@gmail.com>
2024-10-18 08:08:31 -0600

    works now

--- index.ts
+++ index.ts
@@ -1,24 +1,23 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-// // Download the helper library from https://www.twilio.com/docs/node/install
-// import twilio from "twilio"; // Or, for ESM: import twilio from "twilio";
+// Download the helper library from https://www.twilio.com/docs/node/install
+import twilio from "twilio";
 
-// // Find your Account SID and Auth Token at twilio.com/console
-// // and set the environment variables. See http://twil.io/secure
-// const accountSid = process.env.TWILIO_ACCOUNT_SID;
-// const authToken = process.env.TWILIO_AUTH_TOKEN;
-// const client = twilio(accountSid, authToken);
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(accountSid, authToken);
 
-// async function createMessage() {
-// 	const message = await client.messages.create({
-// 		body: "From my code",
-// 		from: "+14807085773",
-// 		to: "+18446209126",
-// 	});
+async function createMessage() {
+	const message = await client.messages.create({
+		body: "From my code",
+		from: "+18446209126",
+		to: "+18777804236",
+	});
 
-// 	console.log(message.body);
-// }
+	console.log(message.body);
+}
 
-// createMessage();
-console.log("Hello", process.env.TWILIO_ACCOUNT_SID);
+createMessage();
